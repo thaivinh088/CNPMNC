594c4426ed01b7d4b875eed51163baf9d7fe2ee7
thaivinh088 <thaivinhnguyen088@gmail.com>
2022-04-08 14:42:05 +0700

    update

---
+++ LAB5/LAB5/Controllers/DEPARTMENTsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LAB5.Models;
+
+namespace LAB5.Controllers
+{
+    public class DEPARTMENTsController : Controller
+    {
+        private SalesERPDALEntities db = new SalesERPDALEntities();
+
+        // GET: DEPARTMENTs
+        public ActionResult Index()
+        {
+            return View(db.DEPARTMENTs.ToList());
+        }
+
+        // GET: DEPARTMENTs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DEPARTMENT dEPARTMENT = db.DEPARTMENTs.Find(id);
+            if (dEPARTMENT == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dEPARTMENT);
+        }
+
+        // GET: DEPARTMENTs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DEPARTMENTs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "DepartmentID,DepartmentName,Location")] DEPARTMENT dEPARTMENT)
+        {
+            if (ModelState.IsValid)
+            {
+                db.DEPARTMENTs.Add(dEPARTMENT);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(dEPARTMENT);
+        }
+
+        // GET: DEPARTMENTs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DEPARTMENT dEPARTMENT = db.DEPARTMENTs.Find(id);
+            if (dEPARTMENT == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dEPARTMENT);
+        }
+
+        // POST: DEPARTMENTs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "DepartmentID,DepartmentName,Location")] DEPARTMENT dEPARTMENT)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(dEPARTMENT).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(dEPARTMENT);
+        }
+
+        // GET: DEPARTMENTs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DEPARTMENT dEPARTMENT = db.DEPARTMENTs.Find(id);
+            if (dEPARTMENT == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dEPARTMENT);
+        }
+
+        // POST: DEPARTMENTs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            DEPARTMENT dEPARTMENT = db.DEPARTMENTs.Find(id);
+            db.DEPARTMENTs.Remove(dEPARTMENT);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
